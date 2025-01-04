27ba596b572de11c10dc9dee033c671467475eab
Khalid-MahmouD <khaloooood827@gmail.com>
2025-01-04 18:55:02 +0200

    static files

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import HomeCards from './components/HomeCards.jsx'
+import JobListing from "./components/JobListings.jsx"
+import ViewAll from './components/ViewAll.jsx'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Hero />
+
+      <HomeCards />
+
+      <JobListing />
+
+      <ViewAll />
+
+
+    </>
+  )
+}
+
+export default App
